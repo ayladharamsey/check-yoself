fbb967aac93caf5dd66085f37db68d778322878d
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-06-05 06:54:09 -0600

    Updates made to the css slightly

--- todo-list.js
+++ todo-list.js
@@ -1,38 +1,48 @@
 class ToDoList {
 	constructor(id, title, tasks, urgent) {
+
 		this.id = id || Date.now();
 		this.title = title;
 		this.taskList = tasks;
 		this.urgent = urgent || false;
+
 	}
 
 	saveToStorage() {
+
 		var stringifyDataArray = JSON.stringify(todoCards);
       	localStorage.setItem("todos", stringifyDataArray);
+
     };
 
 	deleteFromStorage(index) {
+
 		todoCards.splice(index, 1);
 		this.saveToStorage();
+
 	}
 
 	updateToDo() {
+
 		this.urgent = !this.urgent;
 		this.saveToStorage(); 
+
 
 	}
 
 	updateTask(index) {
+
 		this.taskList[index].taskComplete = !this.taskList[index].taskComplete
 		this.saveToStorage();
+
 	}
 }
 
 class Items {
-  constructor(body, id) {
-    this.body = body;
-    this.taskComplete = false;
-    this.id = id || Date.now();
+	constructor(body, id) {
+	    this.body = body;
+	    this.taskComplete = false;
+	    this.id = id || Date.now();
   }
 
 }
